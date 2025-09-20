7e45d550c7bffee044133998f1f430b7847beb71
Justin Dubois <159587062+TheGhoulBoy@users.noreply.github.com>
2025-09-20 17:09:11 -0400

    Création de la page terms

---
+++ frontend/src/app/terms/page.tsx
@@ -0,0 +1,9 @@
+import styles from "../styles/page.module.css";
+
+export default function Home() {
+  return (
+    <div>
+        <h1 className={styles.italic}>Page d'accueil</h1>
+    </div>
+  );
+}
