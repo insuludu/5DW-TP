74f05f03419244f9851145c575ef74b89364790f
Simon Dery <simondery21@gmail.com>
2025-10-10 23:07:47 -0400

    Controller pour récuper le contenu d'une image

---
+++ backend/backend/backend/Controllers/ImageController.cs
@@ -0,0 +1,36 @@
+﻿using backend.Models;
+using backend.Models_DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+        public ImageController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id}")]
+        [Produces("image/png", "image/jpeg")]
+        [ResponseCache(Duration = 3600, Location = ResponseCacheLocation.Any)]
+        public ActionResult Get(int id)
+        {
+            var img = _context.ProductImages.Where(pi => pi.Id == id)
+                .Select(pi => new {
+                    pi.ContentType,
+                    pi.ImageData
+                }).FirstOrDefault();
+
+            if (img == null || img.ImageData == null)
+                return NotFound();
+
+            return File(img.ImageData, img.ContentType);
+        }
+
+    }
+}
