cff7f5283bb4641b0e3c48bad98704dc89aeae74
Simon Dery <simondery21@gmail.com>
2025-10-19 22:55:35 -0400

    Update category-card.tsx

--- frontend/src/app/home/category-card.tsx
+++ frontend/src/app/home/category-card.tsx
@@ -2,7 +2,7 @@ import styles from "../styles/page.module.css"
 
 export default function CategoryCard({categoryName, id} : { categoryName : string, id: number}) {
     return(
-        <a href={`/shop?categories=${categoryName}`} className={`m-2 rounded-5 text-center p-2 category-card ${styles.categoryCard} ${styles.backgroundThird}`}>
+        <a href={`/shop?categories=${id}`} className={`m-2 rounded-5 text-center p-2 category-card ${styles.categoryCard} ${styles.backgroundThird}`}>
             <span className="fs-6" >{categoryName}</span>
         </a>
     );
