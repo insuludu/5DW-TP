d53cdeeefbd84298fe3830b8d4304f21ed931766
Skyjack880 <jacob07man@gmail.com>
2025-10-27 10:57:30 -0400

    bro :(

--- frontend/src/app/shop/product/[id]/page.tsx
+++ frontend/src/app/shop/product/[id]/page.tsx
@@ -110,7 +110,7 @@ export default async function Page({ params }: { params: { id: string } }) {
                                 Panier temporairement indisponible
                             </button>
                             <a 
-                                href={`/edit-product?${product.id}`}
+                                href={`/edit-product?id=${product.id}`}
                                 className="btn btn-outline-primary btn-lg d-flex align-items-center justify-content-center gap-2"
                             >
                                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
